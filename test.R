0ff6398cafb9396ea5f2e4168cd54d16776a1bff
Federico Bonofiglio <federico.bonofiglio@cytel.com>
2026-02-22 10:43:47 +0000

    test interaction effect

--- test.R
+++ test.R
@@ -23,3 +23,51 @@ mean(
 
 
 
+############### INTERACTION #################
+
+
+interaction <- function(n = 1000,
+                        beta0 = 1,
+                        beta1 = 2,
+                        betatrt = -10,
+                        betaint = 5,
+                        sigma0 = 1,
+                        fx = function(x) rgamma(x, 60) # rbinom(x, 1, 0.)
+                        )
+{
+  
+  set.seed(12)
+  
+  n <-  1000
+  
+  # simulate predictor
+  x <- fx(n)
+  
+  # random allocation
+  a <- rbinom(n, 1, 0.5)
+  
+  # mu
+  mu <- beta0 + beta1*x + betaint*x*a + betatrt*a
+  
+  y <- rnorm(n, mu, sigma0)
+  
+  data.frame(y=y, trt=a, x=x)
+  
+}
+
+
+dd <- interaction()
+
+# change predictor dist
+dd2 <- interaction(
+  fx = function(x) rgamma(x, 20)
+)
+
+
+summary(lm(y~trt, data = dd))
+
+summary(lm(y~trt + x, data = dd))
+
+summary(lm(y~x*trt, data = dd))
+
+
