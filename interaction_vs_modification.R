d9bec8ada006a4bd38ae4ca89bae23b53d84b0ce
Federico Bonofiglio <federico.bonofiglio@cytel.com>
2026-03-13 18:36:55 +0000

    OK

--- interaction_vs_modification.R
+++ interaction_vs_modification.R
@@ -69,7 +69,13 @@ modification <- function(n = 1000,
 
 ########## UNDERSTAND THE DIFFERENCE IN THE ANALYSIS ############
 
-# CONCEPTUALLY, IT IS NOT EASY TO CONTROL THE "TRUE" SUBGROUP EFFECT USING A STATISTICAL INTERACTION FOR THE DATA GENERATING MODEL, BECAUSE THERE IS NO INTUITIVE TRANSLATION OF THE MODEL PARAMETERS (interaction term and direct effects) INTO "TRUE" AVERAGE AND SUBGROUP EFFECTS.
+# INTERACTION
+# average direct effect = -10
+# effect in subgroup = ? 
+# average direct effect of interactor = 2
+# interaction term = 2
+
+# IT IS NOT EASY TO CONTROL THE "TRUE" SUBGROUP EFFECT USING A STATISTICAL INTERACTION FOR THE DATA GENERATING MODEL, BECAUSE THERE IS NO INTUITIVE TRANSLATION OF THE MODEL PARAMETERS (interaction term and direct effects) INTO "TRUE" AVERAGE AND SUBGROUP EFFECTS.
 
 # continuous interaction term
 dat_int <- interaction()
@@ -116,23 +122,26 @@ summary(lm(y~trt, data = dat_int2, subset = x == "1"))
 
 
 ##### EFFECT MODIFICATION #######
+# average effect = -10
+# effect in A = 20
+# effect in B = -40
+
 
 dat_mod <- modification()
 
-# average TRT effect strongly biased if interaction not accounted for !!!
+# unbiased average TRT effect 
 summary(lm(y~trt, data = dat_mod))
 
-
+# slightly biased average TRT effect for higher R^2 if adjusting for modifier
 summary(lm(y~trt + x, data = dat_mod))
 
-# only accounting for interaction term returns unbiased trt estimates
-summary(lm(y~x*trt, data = dat_int))
+# statistical interaction highlight modification effect but returns biased estimates.
+# In fact, in this clean scenario (no other prognostic variables) the TRT effect is equal to the effect in group A and the interaction is equal to the contrast between group B and A (B-A), which means the actual (unbiased) effect in group B is trt + interaction. Therefore, all parameters are thrown all over but still interpretable if put in correct slot. However, adding another covariate in this model will makes this disentagling almost impossible
+summary(lm(y~x*trt, data = dat_mod))
 
-# SUBGROUP analysis: detects effect modification but estimates are always biased !
-# on continuous scale: not clear what is subgroup cut-off to observe modification
+# SUBGROUP analysis: unbiased subgroup effects whose average correctly returns the true average TRT effect
 
-# cutting off on mean value of interaction variable: no adjustment for interaction variable
-summary(lm(y~trt, data = dat_int, subset = x < 60))
-summary(lm(y~trt, data = dat_int, subset = x >= 60))
+summary(lm(y~trt, data = dat_mod, subset = x == "A"))
+summary(lm(y~trt, data = dat_mod, subset = x == "B"))
 
 
