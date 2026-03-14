c94139959d20932f1bfdfbcef0c410c27d16d8c5
Federico Bonofiglio <federico.bonofiglio@cytel.com>
2026-03-14 07:43:11 +0000

    clarify interaction term analysis with trt modifier ...

--- R/trial_simul.R
+++ R/trial_simul.R
@@ -99,30 +99,29 @@ dat3 <- trial_simul(N = 1000, delta = -10, mu0 = 20, beta = 2,
 
 # estimator
 
-# total effect
+# average effect is slightly biased due to unequal prevalence of the modifier strata. This bias can be addressed by adjusting for the modifier (via subgroup analysis or interaction term) to obtain the unbiased stratum specific effect whose average is the true average trt effect. Any prognostic variable must be accounted for to increase precision
 summary(
   lm(y~trt, data = dat)
 )
 
-# subgroup
+# subgroup analysis returns true subgroup effects but slighly biased if prognostic factor, X, not accounted for
 
 summary(
-  lm(y~trt, data = dat, subset = V1 == 1)
+  lm(y~trt + x, data = dat, subset = V1 == 1)
 )
 
 summary(
-  lm(y~trt, data = dat, subset = V2 == 1)
+  lm(y~trt + x, data = dat, subset = V2 == 1)
 )
 
 summary(
-  lm(y~trt, data = dat, subset = V3 == 1)
+  lm(y~trt + x, data = dat, subset = V3 == 1)
 )
 
 
 #################
 
-# including an interaction with the moderator transfer
-# all signals on the interaction terms: marginal effect is washed out
+# including an interaction with the moderator and adjusting for prognostic factor X is akin to performing the subgroup analysis above ...
 
 summary(
   lm(y~trt + V, data = dat)
@@ -138,7 +137,7 @@ summary(
 )
 
 
-# the interaction terms are the V2-V1 = 10 and V3-V1 = -40 contrasts respectively. Summing the interaction term with trt yields the stratum specific effect (trt = effect of V1)
+# the interaction terms are the V2-V1 = 10 and V3-V1 = -40 contrasts respectively. Summing the interaction term with trt yields the stratum specific effect (trt = effect of V1). 
 summary(
   lm(y~trt + V*trt + x, data = dat)
 )
