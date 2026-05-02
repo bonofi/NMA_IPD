f766362ae0c35188ae75a4476f79d477415b83de
Federico Bonofiglio <federico.bonofiglio@cytel.com>
2026-05-02 11:21:17 +0000

    failed convergence tzpicallz due to cor(V1,V2) = 1. Dropping V1

--- R/gcipdr_ipw_balance.R
+++ R/gcipdr_ipw_balance.R
@@ -43,7 +43,10 @@ do_gcipdr <- function(
       dplyr::select(
         y, trt, x, starts_with("v")
       ) |> 
-      dplyr::select(where(is.numeric))
+      dplyr::select(where(is.numeric)) |> 
+      # drop redundant reference stratum level because 
+      # it might cause trouble during optimization (corr values close to boundary)
+      dplyr::select(!any_of("V1"))
   )
   
   # generate pseudodata. Output: list with boot repetition by study. Need to reorganize as list of pooled-by-study data repetitions  
@@ -59,12 +62,44 @@ do_gcipdr <- function(
         method = 3, # NORTA with Gamma marginals and Pearson corr
         checkdata = TRUE, 
         tabulate.similar.data = TRUE, 
+        stochastic.integration = FALSE,
         NI_maxEval = 0)
     )
  
      )
   names(raw) <- unique(ipd_network$study)
   
+  # check if any GC failed with numericla integration only
+  fails <- which(
+    unlist(
+      lapply(raw, function(x) class(x))
+    ) == "try-error"
+  )
+    
+
+  # if some fails, rerun with MC integration
+  if (length(fails) > 0){
+    for (i in fails)
+      print( 
+        system.time(
+          raw[[i]] <- gcipdr::Simulate.many.datasets(
+            input[i],
+            H = boot_iter, 
+            method = 3, # NORTA with Gamma marginals and Pearson corr
+            checkdata = TRUE, 
+            tabulate.similar.data = TRUE,
+            stochastic.integration = TRUE,
+            SI_k = 20000
+
+            )
+        )
+        
+      )
+        
+  }
+      
+  
+    
     
   # pool pseudodata by study
   
