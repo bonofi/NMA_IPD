466affc0caf3966f96ed3864c1d2ea8168320c36
Federico Bonofiglio <federico.bonofiglio@cytel.com>
2026-05-27 14:45:28 +0000

    check ongoing

--- checkGCres.R
+++ checkGCres.R
@@ -1,23 +1,74 @@
 #' check GC performance with IPW
 
-
+ref <- res1dat |> filter(inconsistency == "high" & samplesize == "large")
 dat <- rawGC4$high$large$pseud[[1]]
 
+dat <- rawGCtest3$pseud[[1]]
 
-prova <- ipw_balance(
-  ipd_network = dat,
+provaref <- ipw_balance(
+  ipd_network = ref,
   model_formula = as.formula(study ~ x + V1 + V2),
   estimand = "ATT",
   stop_rule = "es.mean"
 )
 
-prova$rawres |> print()
-
-
-prova2 <- ipw_balance(
-  ipd_network = dat,
+prova <- ipw_balance(
+  ipd_network = dat ,#|> filter(study != "5"),
   model_formula = as.formula(study ~ x + V1 + V2),
   estimand = "ATT",
-  stop_rule = "es.mean",
-  n_trees = 10000
+  stop_rule = "ks.mean",
+  n_trees = 3000
 )
+
+prova$rawres |> print()
+
+# effect in study 3, while consistent with 1, is not properly reproduced in GC ... But after discarding 3, it appears to GC cannot well balance across studies --> residual inconsistency
+prova2 <- ipw_balance(
+  ipd_network = dat |> filter(study %in% c("1", "5", "4")),
+  model_formula = as.formula(study ~ x + V1 + V2),
+  estimand = "ATT",
+  stop_rule = "ks.mean"
+)
+
+
+
+
+
+
+
+####### rerun GC with different V level 
+
+system.time(
+  rawGCtest3 <- do_gcipdr(
+    ipd_network = dat,
+    boot_iter = 10,
+    method = "4",
+    SI_k = 60000,
+    only_SI = TRUE,
+    seed = 30697,
+    cores = 6,
+    drop_ref_V = "V2"
+  )
+)
+#########
+
+
+
+
+res <- twang::mnps(
+  as.formula(study ~ x + V1 + V2),
+  data = dat |> mutate(study = as.factor(study)),
+  estimand = "ATT",
+  treatATT = "1",
+  interaction.depth = 10,
+  verbose = FALSE,
+  stop.method = "es.mean",
+  n.trees = 3000)
+
+## weighted regression according to twang documentation: SE are different
+dat$w <- twang::get.weights(res, stop.method = "es.mean")
+datdes <- survey::svydesign(ids=~1, weights=~w, data=dat)
+
+glm1 <- survey::svyglm(y ~ as.factor(trt_name), design = datdes)
+summary(glm1)
+
