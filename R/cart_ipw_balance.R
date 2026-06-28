9b3dc9a871851fd0665ecfa60f4d929366ce3840
Federico Bonofiglio <federico.bonofiglio@cytel.com>
2026-06-28 05:00:56 +0000

    cart small sample fails

--- R/cart_ipw_balance.R
+++ R/cart_ipw_balance.R
@@ -5,14 +5,41 @@
 
 
 cart_ipw_balance <- function(
-    ipd_network
+    ipd_network,
+    boot_iter = 100,
+    seed = 7385,
+    cores = 5
     
 ){
   
   
   browser()
   
+  future::plan(multisession, workers = cores)
+  
+  tictoc::tic()
+  # synthetic data using original IPD 
+  
+  raw <- ipd_network |> 
+    dplyr::select(
+      y, study, V, trt_name, x) |> 
+ synthpop::syn.strata(
+        strata = "study",
+        m=boot_iter, 
+        seed = seed,
+        minstratumsize = 10
+        )
+    )
+  
+  tictoc::toc()
+  
+  
+  
 }
-  
-  
-  
+
+
+
+cart_ipw_balance(
+  res1dat |> 
+      filter(samplesize == "small" & inconsistency == "high")
+)
