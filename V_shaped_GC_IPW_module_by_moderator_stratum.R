846e049d5111bc97038ca8f8eb489831e289fb84
Federico Bonofiglio <federico.bonofiglio@cytel.com>
2026-08-06 12:19:03 +0000

    test code strata gc run

--- V_shaped_GC_IPW_module_by_moderator_stratum.R
+++ V_shaped_GC_IPW_module_by_moderator_stratum.R
@@ -7,79 +7,181 @@
 ## GC stratified by the mediator variable: proof of concept
 
 B <- 300
+########################################################
+########################################################
+
 
 system.time(
   
-  rawGC3_strata <- lapply(
-    c("level_1", "level_2"),
-    function(l) 
-      
-      split(
-        res1dat |> 
-          dplyr::filter(V == l) |> 
-          dplyr::select(!starts_with("V")), 
-        res1dat$inconsistency[res1dat$V == l]
-      ) |> 
-      purrr::map(
-        \(df1) split(df1, df1$samplesize) |> 
-          purrr::map(
-            \(df2){
-              
-              print(
-                paste0(
-                  "inconsistency ", 
-                  unique(df2$inconsistency), 
-                  "; sample size ", unique(df2$samplesize)
-                )
-              )
-              
-              gc()
-              
-              do_gcipdr(
-                ipd_network = df2,
-                boot_iter = B,
-                method = "3",
-                SI_k = 30000,
-                only_SI = TRUE,
-                seed = 30697,
-                cores = detectCores()-2
-              )
-              
-            }
-            
-            
-          )
-      ) 
-  )
- 
-) 
-
-names(rawGC3_strata) <- c("level_1", "level_2")
-
-# regroup by mediator strata
-
-rawGC3 <- names(rawGC3_strata[[1]]) |> 
-  purrr::map(
-      \(i) names(rawGC3_strata[[1]][[i]]) |> 
+  rawGC3_strata <- split(res1dat, res1dat$inconsistency) |> 
+    purrr::map(
+      \(df1) split(df1, df1$samplesize) |> 
         purrr::map(
-          \(j) 1:B |> 
-            purrr::map(
-              \(b) rawGC3_strata[["level_1"]][[i]][[j]][[b]] |> 
-                dplyr::select(!starts_with("V")) |> 
-                tibble::add_column(
-                  V1 = 1, V2 = 0
-                ) |> 
-                dplyr::bind_rows(
-                  rawGC3_strata[["level_2"]][[i]][[j]][[b]] |> 
-                    dplyr::select(!starts_with("V")) |> 
-                    tibble::add_column(
-                      V1 = 0, V2 = 1
-                    )
-                    
-                )
+          \(df2){
+            
+            print(
+              paste0(
+                "inconsistency ", 
+                unique(df2$inconsistency), 
+                "; sample size ", unique(df2$samplesize)
+              )
             )
+            
+            gc()
+            
+            # stratify by moderator
+            out <- lapply(
+              sort(unique(df2$V)), 
+              function(l) {
+                
+                gc()
+                
+                do_gcipdr(
+                  ipd_network = df2 |> 
+                    dplyr::filter(V == l) |> 
+                    dplyr::select(!starts_with("V")),
+                  boot_iter = B,
+                  method = "3",
+                  SI_k = 30000,
+                  only_SI = TRUE,
+                  seed = 30697,
+                  cores = detectCores()-2
+                )
+              }
+            )
+            names(out) <- sort(unique(df2$V))
+            
+            # conjunct strata into one single data set
+            browser()
+            
+            Vdat <- diag(length(unique(df2$V)))
+            rownames(Vdat) <- names(out)
+            colnames(Vdat) <- stringr::str_replace_all(
+              names(out), pattern = "level_", "V"
+            )
+            
+            pseud2 <- lapply(
+              1:B,
+              function(b)
+                lapply(
+                  names(out),
+                  function(l)
+                    out[[l]]$pseud[[b]] |> 
+                    dplyr::select(
+                      !dplyr::starts_with("V")
+                    ) |> 
+                    tibble::add_column(
+                      !!!as.list(
+                        Vdat[l, ]
+                      )
+                    )
+                ) |> 
+                dplyr::bind_rows() |> 
+                dplyr::arrange(study)
+              
+              # out$level_1$pseud[[b]] |> 
+              # dplyr::select(
+              #   !dplyr::starts_with("V")
+              # ) |> 
+              # tibble::add_column(V1 = 1, V2 = 0) |>  # replace with bang-bang and diagonal 0-1 V matrix ?
+              # dplyr::bind_rows(
+              #   out$level_2$pseud[[b]] |> 
+              #     dplyr::select(
+              #       !dplyr::starts_with("V")
+              #     ) |> 
+              #     tibble::add_column(V1 = 0, V2 = 1)
+              # ) |> 
+                # dplyr::arrange(study)
+            ) 
+            
+            return(
+              list(
+                raw = lapply(out, function(x) x$raw ),
+                pseud = pseud
+              )
+            )
+          }     
         )
-    )
+    ) 
+)
+
+
+
+
+# DEPRECATED
+# ###############################################
+# system.time(
+#   
+#   rawGC3_strata <- lapply(
+#     c("level_1", "level_2"),
+#     function(l)
+#       
+#       split(
+#         res1dat |> 
+#           dplyr::filter(V == l) |> 
+#           dplyr::select(!starts_with("V")), 
+#         res1dat$inconsistency[res1dat$V == l]
+#       ) |> 
+#       purrr::map(
+#         \(df1) split(df1, df1$samplesize) |> 
+#           purrr::map(
+#             \(df2){
+#               
+#               print(
+#                 paste0(
+#                   "inconsistency ", 
+#                   unique(df2$inconsistency), 
+#                   "; sample size ", unique(df2$samplesize)
+#                 )
+#               )
+#               
+#               gc()
+#               
+#               do_gcipdr(
+#                 ipd_network = df2,
+#                 boot_iter = B,
+#                 method = "3",
+#                 SI_k = 30000,
+#                 only_SI = TRUE,
+#                 seed = 30697,
+#                 cores = detectCores()-2
+#               )
+#               
+#             }
+#             
+#             
+#           )
+#       ) 
+#   )
+#   
+# ) 
+# 
+# names(rawGC3_strata) <- c("level_1", "level_2")
+# 
+# # regroup by mediator strata
+# 
+# rawGC3 <- names(rawGC3_strata[[1]]) |> 
+#   purrr::map(
+#       \(i) names(rawGC3_strata[[1]][[i]]) |> 
+#         purrr::map(
+#           \(j) 1:B |> 
+#             purrr::map(
+#               \(b) rawGC3_strata[["level_1"]][[i]][[j]][[b]] |> 
+#                 dplyr::select(!starts_with("V")) |> 
+#                 tibble::add_column(
+#                   V1 = 1, V2 = 0
+#                 ) |> 
+#                 dplyr::bind_rows(
+#                   rawGC3_strata[["level_2"]][[i]][[j]][[b]] |> 
+#                     dplyr::select(!starts_with("V")) |> 
+#                     tibble::add_column(
+#                       V1 = 0, V2 = 1
+#                     )
+#                     
+#                 )
+#             )
+#         )
+#     )
 
 
 #### RUN GC-IPW IPD-AD (reference study nr 1 is in IPD format, all other are AD)
