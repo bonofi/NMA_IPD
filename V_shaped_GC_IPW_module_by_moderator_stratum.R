23610e4f8e7ff7e5baf076733c48eafe6fd38cfa
Federico Bonofiglio <federico.bonofiglio@cytel.com>
2026-08-06 14:01:20 +0000

    clean

--- V_shaped_GC_IPW_module_by_moderator_stratum.R
+++ V_shaped_GC_IPW_module_by_moderator_stratum.R
@@ -8,8 +8,6 @@
 
 ########################################################
 ########################################################
-
-
 system.time(
   
   rawGC3_strata <- split(res1dat, res1dat$inconsistency) |> 
@@ -27,11 +25,10 @@ system.time(
             )
             
             gc()
-            
             B <- 300
             # stratify by moderator
             out <- lapply(
-              c("level_1", "level_2"), # replace with levels
+              sort(unique(df2$V)), 
               function(l) {
                 
                 gc()
@@ -49,26 +46,36 @@ system.time(
                 )
               }
             )
-            names(out) <- c("level_1", "level_2")
+            names(out) <- sort(unique(df2$V))
             
             # conjunct strata into one single data set
+            
+            # matrix of moderator dummy variables
+            Vdat <- diag(length(unique(df2$V)))
+            rownames(Vdat) <- names(out)
+            colnames(Vdat) <- stringr::str_replace_all(
+              names(out), pattern = "level_", "V"
+            )
             
             pseud <- lapply(
               1:B,
               function(b)
-                out$level_1$pseud[[b]] |> 
-                dplyr::select(
-                  !dplyr::starts_with("V")
-                ) |> 
-                tibble::add_column(V1 = 1, V2 = 0) |>  # replace with bang-bang and diagonal 0-1 V matrix ?
-                dplyr::bind_rows(
-                  out$level_2$pseud[[b]] |> 
+                lapply(
+                  names(out),
+                  function(l)
+                    out[[l]]$pseud[[b]] |> 
                     dplyr::select(
                       !dplyr::starts_with("V")
                     ) |> 
-                    tibble::add_column(V1 = 0, V2 = 1)
+                    tibble::add_column(
+                      !!!as.list(
+                        Vdat[l, ]
+                      )
+                    )
                 ) |> 
-                arrange(study)
+                dplyr::bind_rows() |> 
+                dplyr::arrange(study)
+              
             ) 
             
             return(
@@ -82,88 +89,7 @@ system.time(
     ) 
 )
 
-
-
-
-
-# ###############################################
-# system.time(
-#   
-#   rawGC3_strata <- lapply(
-#     c("level_1", "level_2"),
-#     function(l)
-#       
-#       split(
-#         res1dat |> 
-#           dplyr::filter(V == l) |> 
-#           dplyr::select(!starts_with("V")), 
-#         res1dat$inconsistency[res1dat$V == l]
-#       ) |> 
-#       purrr::map(
-#         \(df1) split(df1, df1$samplesize) |> 
-#           purrr::map(
-#             \(df2){
-#               
-#               print(
-#                 paste0(
-#                   "inconsistency ", 
-#                   unique(df2$inconsistency), 
-#                   "; sample size ", unique(df2$samplesize)
-#                 )
-#               )
-#               
-#               gc()
-#               
-#               do_gcipdr(
-#                 ipd_network = df2,
-#                 boot_iter = B,
-#                 method = "3",
-#                 SI_k = 30000,
-#                 only_SI = TRUE,
-#                 seed = 30697,
-#                 cores = detectCores()-2
-#               )
-#               
-#             }
-#             
-#             
-#           )
-#       ) 
-#   )
-#   
-# ) 
-# 
-# names(rawGC3_strata) <- c("level_1", "level_2")
-# 
-# # regroup by mediator strata
-# 
-# rawGC3 <- names(rawGC3_strata[[1]]) |> 
-#   purrr::map(
-#       \(i) names(rawGC3_strata[[1]][[i]]) |> 
-#         purrr::map(
-#           \(j) 1:B |> 
-#             purrr::map(
-#               \(b) rawGC3_strata[["level_1"]][[i]][[j]][[b]] |> 
-#                 dplyr::select(!starts_with("V")) |> 
-#                 tibble::add_column(
-#                   V1 = 1, V2 = 0
-#                 ) |> 
-#                 dplyr::bind_rows(
-#                   rawGC3_strata[["level_2"]][[i]][[j]][[b]] |> 
-#                     dplyr::select(!starts_with("V")) |> 
-#                     tibble::add_column(
-#                       V1 = 0, V2 = 1
-#                     )
-#                     
-#                 )
-#             )
-#         )
-#     )
-
-
 #### RUN GC-IPW IPD-AD (reference study nr 1 is in IPD format, all other are AD)
-
-
 
 ## run IPW
 
--- back_up_V_shapefGC_strata.R
+++ back_up_V_shapefGC_strata.R
@@ -1,4 +1,6 @@
 
+
+
 system.time(
   
   rawGC3_strata <- split(res1dat, res1dat$inconsistency) |> 
@@ -16,10 +18,11 @@ system.time(
             )
             
             gc()
+            
             B <- 300
             # stratify by moderator
             out <- lapply(
-              sort(unique(df2$V)), 
+              c("level_1", "level_2"), # replace with levels
               function(l) {
                 
                 gc()
@@ -37,49 +40,26 @@ system.time(
                 )
               }
             )
-            names(out) <- sort(unique(df2$V))
+            names(out) <- c("level_1", "level_2")
             
             # conjunct strata into one single data set
-            
-            # matrix of moderator dummy variables
-            Vdat <- diag(length(unique(df2$V)))
-            rownames(Vdat) <- names(out)
-            colnames(Vdat) <- stringr::str_replace_all(
-              names(out), pattern = "level_", "V"
-            )
             
             pseud <- lapply(
               1:B,
               function(b)
-                lapply(
-                  names(out),
-                  function(l)
-                    out[[l]]$pseud[[b]] |> 
+                out$level_1$pseud[[b]] |> 
+                dplyr::select(
+                  !dplyr::starts_with("V")
+                ) |> 
+                tibble::add_column(V1 = 1, V2 = 0) |>  # replace with bang-bang and diagonal 0-1 V matrix ?
+                dplyr::bind_rows(
+                  out$level_2$pseud[[b]] |> 
                     dplyr::select(
                       !dplyr::starts_with("V")
                     ) |> 
-                    tibble::add_column(
-                      !!!as.list(
-                        Vdat[l, ]
-                      )
-                    )
+                    tibble::add_column(V1 = 0, V2 = 1)
                 ) |> 
-                dplyr::bind_rows() |> 
-                dplyr::arrange(study)
-              
-              # out$level_1$pseud[[b]] |> 
-              # dplyr::select(
-              #   !dplyr::starts_with("V")
-              # ) |> 
-              # tibble::add_column(V1 = 1, V2 = 0) |>  # replace with bang-bang and diagonal 0-1 V matrix ?
-              # dplyr::bind_rows(
-              #   out$level_2$pseud[[b]] |> 
-              #     dplyr::select(
-              #       !dplyr::starts_with("V")
-              #     ) |> 
-              #     tibble::add_column(V1 = 0, V2 = 1)
-              # ) |> 
-              # dplyr::arrange(study)
+                arrange(study)
             ) 
             
             return(
@@ -92,3 +72,83 @@ system.time(
         )
     ) 
 )
+
+
+
+
+
+# ###############################################
+# system.time(
+#   
+#   rawGC3_strata <- lapply(
+#     c("level_1", "level_2"),
+#     function(l)
+#       
+#       split(
+#         res1dat |> 
+#           dplyr::filter(V == l) |> 
+#           dplyr::select(!starts_with("V")), 
+#         res1dat$inconsistency[res1dat$V == l]
+#       ) |> 
+#       purrr::map(
+#         \(df1) split(df1, df1$samplesize) |> 
+#           purrr::map(
+#             \(df2){
+#               
+#               print(
+#                 paste0(
+#                   "inconsistency ", 
+#                   unique(df2$inconsistency), 
+#                   "; sample size ", unique(df2$samplesize)
+#                 )
+#               )
+#               
+#               gc()
+#               
+#               do_gcipdr(
+#                 ipd_network = df2,
+#                 boot_iter = B,
+#                 method = "3",
+#                 SI_k = 30000,
+#                 only_SI = TRUE,
+#                 seed = 30697,
+#                 cores = detectCores()-2
+#               )
+#               
+#             }
+#             
+#             
+#           )
+#       ) 
+#   )
+#   
+# ) 
+# 
+# names(rawGC3_strata) <- c("level_1", "level_2")
+# 
+# # regroup by mediator strata
+# 
+# rawGC3 <- names(rawGC3_strata[[1]]) |> 
+#   purrr::map(
+#       \(i) names(rawGC3_strata[[1]][[i]]) |> 
+#         purrr::map(
+#           \(j) 1:B |> 
+#             purrr::map(
+#               \(b) rawGC3_strata[["level_1"]][[i]][[j]][[b]] |> 
+#                 dplyr::select(!starts_with("V")) |> 
+#                 tibble::add_column(
+#                   V1 = 1, V2 = 0
+#                 ) |> 
+#                 dplyr::bind_rows(
+#                   rawGC3_strata[["level_2"]][[i]][[j]][[b]] |> 
+#                     dplyr::select(!starts_with("V")) |> 
+#                     tibble::add_column(
+#                       V1 = 0, V2 = 1
+#                     )
+#                     
+#                 )
+#             )
+#         )
+#     )
+
+
